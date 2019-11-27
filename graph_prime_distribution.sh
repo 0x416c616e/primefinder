cd853ba0bab52c2a7c4a93d97fefa65ad03051e0
0x416c616e <29168822+0x416c616e@users.noreply.github.com>
2019-11-27 00:29:01 -0600

    finished new prime_distribution.sh script

---
+++ graph_prime_distribution.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+LASTRUN=$(cat last_was_completed.txt | tr -d '\n')
+
+
--- prime_distribution.sh
+++ prime_distribution.sh
@@ -2,9 +2,23 @@
 COUNT=$(ls | grep gaps_ | wc -l | tr -d '\n')
 COUNT=$(($COUNT-1))
 FILESIZE=50000
+if [ $COUNT -le 0 ]
+then
+	echo "error: need more data in order to graph distribution"
+	exit 1
+fi
+
+LASTRUN=$(cat last_was_completed.txt | tr -d '\n')
+if [ $LASTRUN == "false" ]
+then
+	#this means the last file is incomplete and should be disregarded
+	COUNT=$(($COUNT-1))
+fi
+
+
 for (( i=0; i<=$COUNT; i++ ))
 do
-	NAME=$"gaps_$i.csv"
+	NAME=$"gaps_$i.dat"
 	OFFSET=$(($i*$FILESIZE))
 	NUMPRIMES=$(cat $NAME | wc -l | tr -d '\n')
 	echo $OFFSET, $NUMPRIMES
